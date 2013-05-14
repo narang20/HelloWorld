2f7f504d1238eae80f288f34d65ceb368618adfd
narang20 <narang20@gmail.com>
2013-05-14 13:14:51 +0200

    Update Test.java
    deeeee

--- HelloWorld/src/uk/Test.java
+++ HelloWorld/src/uk/Test.java
@@ -3,7 +3,7 @@ package uk;
 public class Test {
 	
 	public static void main(String args[]){
-		System.out.println("hello world newv this is new changedd");
+		System.out.println("hello world newv this is new changedd1");
 	}
 
 }
